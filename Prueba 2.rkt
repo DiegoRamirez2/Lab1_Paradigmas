79b9c1a5abfab63f493bb55fb886479203187a97
Diego Ramírez <diego.ramirez.vi@usach.cl>
2022-04-09 21:19:10 -0400

    Se logra generar un conjunto de las nSquareCards

--- Prueba 2.rkt
+++ Prueba 2.rkt
@@ -8,18 +8,7 @@
 (define firstCard (lambda (lista_elemento)
                           (firstCardAux lista_elemento 0)))
 
-;Función que crea el mazo con las N cartas
-;(define NfirstCard (lambda (lista_elemento)
-;                     (if (= (length lista_elemento))))) 
-
-
-
-(define NfirstCardAux (lambda (lista_elemento numero)
-                         (if (= (length lista_elemento) numero)
-                           null
-                             (cons (createNcardAux lista_elemento numero 1) (list(NfirstCardAux lista_elemento (+ numero 1)))))))
-
-
+;Función que crea el mazo con las N + 1 cartas
 (define createNcard (lambda (lista_elemento)
                          (cons (firstCard lista_elemento) (createNcardAux lista_elemento 1))))
 
@@ -32,14 +21,35 @@
 ; Función que crea las N cartas según fórmula
 ; Crea EJ: (5, 6 ,7) (8, 9 10) (11, 12, 13)
 (define createNcardAux2 (lambda (lista_elemento iterador1 iterador2)
-                       (if (= (+ (simbolsCard (length lista_elemento)) 1) iterador2)
+                       (if (< (simbolsCard (length lista_elemento)) iterador2)
                            null                                                                                        ; Esto esta bueno (iterador2)
                            (cons (list-ref lista_elemento (- (+ (* (- (simbolsCard (length lista_elemento)) 1) iterador1) iterador2) 1)) (createNcardAux2 lista_elemento iterador1 (+ iterador2 1))))))
 
+; Función
+(define SquareCardsAux (lambda (lista_elemento i)
+                         (if (= (simbolsCard (length lista_elemento)) i)
+                             null
+                             (cons (SquareCardsAux2 lista_elemento i 1) (SquareCardsAux lista_elemento (+ i 1)))))) 
+
+; Función que genera una carta perteneciente a la tercera parte del algoritmo dado
+(define SquareCardsAux2 (lambda (lista_elemento i j)
+                       (if (= (simbolsCard (length lista_elemento)) j)
+                           null
+                           (cons (cons (list-ref lista_elemento (+ j 1)) (SquareCardsAux3 lista_elemento i j 1)) (SquareCardsAux2 lista_elemento i (+ j 1))))))
 
 
+; Formula para calcular el n constante: (- (simbolsCard (length lista_elemento)) 1)
+; Función que genera cartas según la fórmula (+ (+ (+ n 2) (* n (- k 1))) (modulo (* (- i 1) (+ (- k 1) (- j 1))) n))))
+(define SquareCardsAux3 (lambda (lista_elemento i j k)
+                          (if (< (- (simbolsCard (length lista_elemento)) 1) k)
+                              null
+                              (cons (list-ref lista_elemento (-(calculoValor (- (simbolsCard (length lista_elemento))1) i j k) 1)) (SquareCardsAux3 lista_elemento i j (+ k 1))))))
 
 
+(define calculoValor (lambda (n i j k)
+                       (+ (+ n 2) (* n (- k 1)) (modulo (+ (* (- i 1) (- k 1)) (- j 1)) n))))
+                       
+                                        
 (define append2 (lambda (lis1 lis2)
   (cond ((null? lis1)
          lis2)
@@ -51,4 +61,6 @@
 (define simbolsCard (lambda (n)
                    (+ (/ (- (sqrt (- (* 4 n) 3)) 1) 2) 1)))
 
-(createNcard (list 1 "Hola" 3 "XD" 5 "Ramo" 7 8 9 24+3i 11 "culiao" 13))
+;(createNcard (list 1 "Hola" 3 "XD" 5 "Ramo" 7 8 9 24+3i 11 "culiao" 13))
+;(createNcard (list 1 2 3 4 5 6 7 8 9 10 11 12 13))
+(SquareCardsAux (list 1 2 3 4 5 6 7 8 9 10 11 12 13) 1)
