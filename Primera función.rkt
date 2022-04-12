137c7e9428b661d5212c69965af46c4600145dcd
DiegoRamirez2 <102471762+DiegoRamirez2@users.noreply.github.com>
2022-04-11 21:12:27 -0400

    Update Primera función.rkt

--- Primera función.rkt
+++ Primera función.rkt
@@ -71,7 +71,7 @@
                        acum
                        ((largoAux (cdr lista) (+ 1 acum))))))
 
-;(createNcard (list 1 "Hola" 3 "XD" 5 "Ramo" 7 8 9 24+3i 11 "culiao" 13))
+;(createNcard (list 1 "Hola" 3 "XD" 5 "Ramo" 7 8 9 24+3i 11 "xddd" 13))
 ;(createNcard (list 1 2 3 4 5 6 7 8 9 10 11 12 13))
 ;(SquareCard (list 1 2 3 4 5 6 7 8 9 10 11 12 13))
 ;(SquareCard (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57))
