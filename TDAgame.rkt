c6075e48635716d9661a63a2eef0c5fb8a22d13c
Diego Ramírez <diego.ramirez.vi@usach.cl>
2022-04-17 11:41:16 -0400

    Posible representación Game

--- TDAgame.rkt
+++ TDAgame.rkt
@@ -8,8 +8,26 @@
 
 (define player (lambda (userName)
                      (list userName null 0 #f)))
+
+(define firstPlayer (lambda (userName)
+                     (list userName null 0 #t)))
+
+                     
 (define register (lambda (user gameSet)
                    (if (= (- (length (car gameSet)) 1) (car (car gameSet)))
                        gameSet
-                       (append (list (append (car gameSet) (list (player user))))
-                           (cdr gameSet)))))
+                       (if (= (length (car gameSet)) 1)
+                           (append (list (append (car gameSet) (list (firstPlayer user))))
+                           (cdr gameSet))
+                           (append (list (append (car gameSet) (list (player user))))
+                           (cdr gameSet))))))
+
+(define whoseTurnsIsIt? (lambda (gameSet)
+                             (whoseTurnsIsItAux? (cdr (car gameSet)))))
+
+(define whoseTurnsIsItAux? (lambda (userList)
+                          (if (car (cdddr userList))
+                              (car (car userList))
+                              (whoseTurnsIsItAux? (cdr userList)))))
+
+                      
